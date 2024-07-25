e8e98bc5c8c7feabc9597995a6ab0a2126f0d9fe
MaheshPuli <pulimahesh34003@gmail.com>
2024-07-25 18:55:15 +0530

    commitday7

--- 02-backend/spring-boot-library/src/main/java/com/universe/spring_boot_library/service/BookService.java
+++ 02-backend/spring-boot-library/src/main/java/com/universe/spring_boot_library/service/BookService.java
@@ -2,8 +2,10 @@ package com.universe.spring_boot_library.service;
 
 import com.universe.spring_boot_library.dao.BookRepository;
 import com.universe.spring_boot_library.dao.CheckoutRepository;
+import com.universe.spring_boot_library.dao.HistoryRepository;
 import com.universe.spring_boot_library.entity.Book;
 import com.universe.spring_boot_library.entity.Checkout;
+import com.universe.spring_boot_library.entity.History;
 import com.universe.spring_boot_library.responsemodels.ShelfCurrentLoansResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,11 +26,13 @@ public class BookService {
 
     private CheckoutRepository checkoutRepository;
 
-    public BookService(BookRepository bookRepository,CheckoutRepository checkoutRepository) {
+    private HistoryRepository historyRepository;
+
+    public BookService(BookRepository bookRepository,CheckoutRepository checkoutRepository, HistoryRepository historyRepository) {
 
         this.bookRepository = bookRepository;
         this.checkoutRepository = checkoutRepository;
-
+        this.historyRepository = historyRepository;
     }
 
     public Book checkoutBook (String userEmail, Long bookId) throws Exception{
@@ -120,6 +124,19 @@ public class BookService {
 
         bookRepository.save(book.get());
         checkoutRepository.deleteById(validateCheckout.getId());
+
+        History history = new History(
+                userEmail,
+                validateCheckout.getCheckoutDate(),
+                LocalDate.now().toString(),
+                book.get().getTitle(),
+                book.get().getAuthor(),
+                book.get().getDescription(),
+                book.get().getImg()
+
+        );
+
+        historyRepository.save(history);
 
     }
 
